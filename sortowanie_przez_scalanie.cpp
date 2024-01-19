14f002e31dc2607e054c098401f29bd1a392e385
Maxio8 <155749540+Maxio8@users.noreply.github.com>
2024-01-19 16:03:59 +0100

    Update sortowanie_przez_scalanie.cpp

--- sortowanie_przez_scalanie.cpp
+++ sortowanie_przez_scalanie.cpp
@@ -23,6 +23,7 @@ void sortowanie_przez_scalanie(int tablica[], int poczatek, int koniec) {
         int srodek = poczatek + (koniec - poczatek) / 2;
         sortowanie_przez_scalanie(tablica, poczatek, srodek);
         sortowanie_przez_scalanie(tablica, srodek + 1, koniec);
+        inplace_merge(tablica + poczatek, tablica + srodek + 1, tablica + koniec + 1);
     }
 }
 
