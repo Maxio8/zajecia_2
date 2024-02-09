a191df68175ee914248c1accd138c6297db4071b
Maxio8 <155749540+Maxio8@users.noreply.github.com>
2024-02-09 13:04:56 +0100

    main_sortowania

---
+++ main_sortowania.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "sortowanie.hpp"
+
+using namespace std;
+
+int main() {
+    int tablica[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(tablica)/sizeof(tablica[0]);
+    sortowaniePrzezWstawianie(tablica, n);
+    cout << "Posortowana tablica: \n";
+    for (int i = 0; i < n; i++)
+        cout << tablica[i] << " ";
+    return 0;
+}
